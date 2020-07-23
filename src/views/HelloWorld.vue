aef6db5c35fe89348d12097f1fc4f71104edf0ef
huyingbin <1416840235@qq.com>
2020-07-23 14:38:34 +0800

    打印表格方法

--- src/views/HelloWorld.vue
+++ src/views/HelloWorld.vue
@@ -1,54 +1,71 @@
 <template>
-<div>
-  <div>{{timestamp | normalTime()}}</div>
-  <div>{{trim | trim(1)}}</div>
-  <div>{{str | changeCase(1)}}</div>
-  <div>{{x | toDecimal2()}}</div>
-  <div>{{hours}}</div>
-  <div>{{welcome}}</div>
-  <div v-time="timeNow"></div>
-</div>
+  <div>
+    <div>{{timestamp | normalTime()}}</div>
+    <div>{{trim | trim(1)}}</div>
+    <div>{{str | changeCase(1)}}</div>
+    <div>{{x | toDecimal2()}}</div>
+    <div>{{hours}}</div>
+    <div>{{welcome}}</div>
+    <div v-time="timeNow"></div>
+  </div>
 </template>
 
 <script>
-import '@/utils/time.js'
-import { normalTime,trim } from '@/utils/filters'
-import { timeFix,welcome,filterObj,randomNumber,randomString,randomUUID,getScrollPosition,clone } from '@/utils/util'
+import "@/utils/time.js";
+import { normalTime, trim } from "@/utils/filters";
+import {
+  timeFix,
+  welcome,
+  filterObj,
+  randomNumber,
+  randomString,
+  randomUUID,
+  getScrollPosition,
+  clone
+} from "@/utils/util";
 export default {
   name: "HelloWorld",
+  components: {
+    
+  },
   data() {
     return {
+      data:'',
       timestamp: "",
-      trim:'傻 浩',
-      str:'xxxxxx',
-      x:566,
-      hours:'',
-      welcome:'',
-      timeNow: '2020-07-23 11:41:00'
+      trim: "傻 浩",
+      str: "xxxxxx",
+      x: 566,
+      hours: "",
+      welcome: "",
+      timeNow: "2020-07-23 11:41:00"
     };
   },
+  
   mounted() {
     this.getTime();
   },
   methods: {
+    onClick(){
+      // console.log(this.data)
+    },
     getTime() {
-      let kk={
-        name:'kk',
-        age:'mm',
-        dd:'11',
-        faves:null,
-        lloe:['xx','aa','as']
-      }
-      console.log(clone(kk))
+      let kk = {
+        name: "kk",
+        age: "mm",
+        dd: "11",
+        faves: null,
+        lloe: ["xx", "aa", "as"]
+      };
+      console.log(clone(kk));
       // let ll=151362980
-      console.log(getScrollPosition())
-      this.hours=timeFix()
-      this.welcome=welcome()
+      console.log(getScrollPosition());
+      this.hours = timeFix();
+      this.welcome = welcome();
       this.timestamp = new Date().getTime();
       console.log(this.timestamp);
-      let aa=normalTime(this.timestamp,'.')
-      let bb=trim(this.trim,1)
-      console.log(aa,bb)
+      let aa = normalTime(this.timestamp, ".");
+      let bb = trim(this.trim, 1);
+      console.log(aa, bb);
     }
   }
 };
